b577b41f70b8c2090de5ae59874d03ccf8568a4b
sai-sreenath <sai-sreenath@users.noreply.github.com>
2021-01-09 21:44:59 -0500

    Design With Hashmap

---
+++ #1244DesignLeaderboard.java
@@ -0,0 +1,39 @@
+class Leaderboard {
+
+    private HashMap<Integer, Integer>scores;
+    
+    public Leaderboard() {
+        this.scores = new HashMap<Integer, Integer>();
+    }
+    
+    public void addScore(int playerId, int score) {
+        if(!this.scores.containsKey(playerId))
+            this.scores.put(playerId, 0);
+        
+        this.scores.put(playerId, this.scores.get(playerId) + score);
+    }
+    
+    public int top(int K) {
+        List<Integer>result = new ArrayList<>(this.scores.values());
+        
+        Collections.sort(result, Collections.reverseOrder());
+        
+        int total = 0;
+        for(int i=0;i<K;i++)
+            total+=result.get(i);
+        
+        return total;
+    }
+    
+    public void reset(int playerId) {
+        this.scores.put(playerId, 0);
+    }
+}
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
