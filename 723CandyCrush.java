ac908452f571ea88d8541515efde9d3c61ee2ad2
sai-sreenath <sai-sreenath@users.noreply.github.com>
2021-02-25 17:08:56 +0530

    candy crush code simple

--- 723CandyCrush.java
+++ 723CandyCrush.java
@@ -1,52 +1,67 @@
 class Solution {
     public int[][] candyCrush(int[][] board) {
         
-        int row = board.length;
-        int col = board[0].length;
-        
-        boolean crush = false;
-        
-        for(int i=0;i<r;i++)
+        //mark -ve for all that are +3 repeat vertical/horizontal_left
+        for(int i=0;i<board.length;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<board[0].length;j++)
             {
-                // check horizontal line
-                int v = Math.abs(board[i][j]);
-                if(v==0)
+                int value = Math.abs(board[i][j]);
+                
+                //check vertical
+                if(i>=2 && value==Math.abs(board[i-1][j]) && value==Math.abs(board[i-2][j]))
+                    board[i][j]=board[i-1][j]=board[i-2][j]=value* -1;
+                
+                //check horizontal
+                if(j>=2 && value==Math.abs(board[i][j-1]) && value==Math.abs(board[i][j-2]))
+                    board[i][j] = board[i][j-1] = board[i][j-2] = value*-1;
+            }
+        }
+     
+        
+        //crush and Drop vertically - traverse from bottom right
+        boolean recursiveCall = false;
+        for(int i=board.length-1; i>=0; i--)
+        {
+            for(int j=board[0].length-1;j>=0;j--)
+            {
+                if(board[i][j]>=0)
                     continue;
-                if(j+2<c && v==Math.abs(board[i][j+1]) && v==Math.abs(board[i][j+2]))
-                {
-                    crush = true;
-                    board[i][j] = board[i][j+1] = board[i][j+2] = -v;
-                }
-                    
-                //check vertical line
-                if(i+2<r && v==Math.abs(board[i+1][j]) && v==Math.abs(board[i+2][j]))
-                {
-                    crush = true;
-                    board[i][j]=board[i+1][j]=board[i+2][j]=-v;
-                }
+                
+                crushAndDropVertically(board,i,j);
+                recursiveCall = true;
             }
         }
         
-        //crush candy
-        for(int j=0; j<c; j++)
-        {
-            int id = r-1;
-            for(int i=r-1;i>=0;i--)
-            {
-                if(board[i][j]>0)
-                    board[id--][j] = board[i][j];
-            }
-            while(id>=0)
-                board[id--][j] = 0;
-        }
-        
-        return crush? candyCrush(board) : board;
+        //print board
+        //return board
+        return recursiveCall? candyCrush(board) : board;
     }
+    
+    void crushAndDropVertically(int[][] board, int i, int j)
+    {
+        //find +ve upwards
+        int k = i-1;
+        for(;k>=0;k--)
+        {
+            if(board[k][j]>0)
+                break;
+        }
+        
+        //keep making +ve drop
+        while(k>=0)
+        {
+            board[i][j] = board[k][j];
+            k--;
+            i--;
+        }
+        
+        //Now fill in zeros vertically
+        while(i>=0)
+        {
+            board[i][j]=0;
+            i--;
+        }
+    }
+    
 }
-
-
-
-
-
